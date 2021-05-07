38ed5d30d6fb93f3725fab6f1506b50c59c9039d
John Berryman <jnbrymn@github.com>
2021-05-06 22:57:28 -0500

    replaced 2 keys with single key to a redis hash

--- lib/earrrl.rb
+++ lib/earrrl.rb
@@ -6,46 +6,49 @@ class Earrrl
   class Error < StandardError; end
 
   # TODO! change the params here
-  # TODO! make sure there is one key that points to a redis set rather than :N and :T
   EARRRL_SCRIPT = <<~LUA
     -- parameters and state
     local lambda = 0.06931471805599453 -- 10 seconds half-life
     local rate_limit = 0.5
     local time_array = redis.call("time")
     local now = time_array[1]+0.000001*time_array[2] -- seconds + microseconds
-    local Nkey = KEYS[1]..":N"
-    local Tkey = KEYS[1]..":T"
-    
-    local N = redis.call("get", Nkey)
-    if N == false then
-      N = 0
+
+    local flat_NT = redis.call("hgetall",KEYS[1])
+    local N = 0.0
+    local T = 0.0
+    if #flat_NT == 4 then
+      if flat_NT[1] == "N" then
+        N = flat_NT[2]
+        T = flat_NT[4]
+      else
+        T = flat_NT[2]
+        N = flat_NT[4]
+      end
     end
-    
-    local T = redis.call("get", Tkey)
-    if T == false then
-      T = 0
-    end
-    
+
     local delta_t = T-now
+
     
     -- functions
+
     local function evaluate()
       return N*lambda*math.exp(lambda*delta_t)
     end
     
     local function update()
-      redis.call("set", Nkey, 1+N*math.exp(lambda*delta_t))
-      redis.call("set", Tkey, now)
+      redis.call("hset", KEYS[1], "N", 1+N*math.exp(lambda*delta_t), "T", now)
     end
     
     local function is_rate_limited()
       local estimated_rate = evaluate()
       local limited = estimated_rate > rate_limit
       update()
-      return {limited, tostring(estimated_rate)}
+      return tostring(estimated_rate)
     end
     
+
     -- the whole big show
+
     return is_rate_limited()
   LUA
 
@@ -56,10 +59,11 @@ class Earrrl
 
 
   # returns estimated rate prior to this update and updates the estimator with this request amount
+  # if not specified, this update amount defaults to 1
   def check(key, update: true, weight: 1)
-    # TODO! connect update and weight to script
-    resp = @redis.evalsha(@earrrl_hash,[1],[key])
-    return !!resp[0], resp[1].to_f
+    resp = @redis.evalsha(@earrrl_hash,[1],[key]).to_f
+    puts "RESPONSE #{resp}"
+    return resp
   end
 
 
--- test/earrrl_test.rb
+++ test/earrrl_test.rb
@@ -12,8 +12,7 @@ class EarrrlTest < Minitest::Test
   def test_check_and_update
     @redis.del("#{@key}:N")
     @redis.del("#{@key}:T")
-    result = @earrrl.check(@key)
-    refute result[0]
-    assert_equal result[1], 0
+    assert @earrrl.check(@key) == 0
+    assert @earrrl.check(@key) > 0
   end
 end
